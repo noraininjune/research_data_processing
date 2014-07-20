db9fb1a18eb20e82ff6b622132d8ff5edee95689
dancingcactus <dancingcactus@gmail.com>
2014-07-19 22:06:32 -0600

    prototype

---
+++ joining.py
@@ -0,0 +1,4 @@
+import pandas as pd
+
+df = pd.read_csv("CombineSheet2.csv")
+df1= pd.read_csv("CombinedNames.csv")
---
+++ process.py
@@ -0,0 +1,26 @@
+import xlrd
+import csv
+
+def txtencode(string):
+    #if type(string) in (unicode, str):
+    string = str(string).replace(u'\xa0', ' ')
+    return string.encode('utf-8')
+    #else:
+        #return string
+
+wb = xlrd.open_workbook('CombinedNamed.xlsx', encoding_override="latin-1")
+sh = wb.sheet_by_name('Combine Sheet')
+your_csv_file = open('CombineSheet2.csv', 'wb')
+wr = csv.writer(your_csv_file, quoting=csv.QUOTE_ALL)
+
+for rownum in xrange(sh.nrows):
+    print rownum
+    row = sh.row_values(rownum)
+    new_row = []
+    for val in row:
+        new_row.append(txtencode(val))
+    wr.writerow(new_row)
+
+your_csv_file.close()
+
+
