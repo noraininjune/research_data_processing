d542978522dc9784441531dcc5f65a632c92f6eb
dancingcactus <dancingcactus@gmail.com>
2014-07-19 22:16:20 -0600

    full script

--- joining.py
+++ joining.py
@@ -2,3 +2,11 @@ import pandas as pd
 
 df = pd.read_csv("CombineSheet2.csv")
 df1= pd.read_csv("CombinedNames.csv")
+
+df1 =df1.rename(columns={'Name':'Name of Sender'})
+df2=pd.merge(df, df1,on='Name of Sender', how='left',suffixes={'','_sender'})
+
+df1= df1.rename(columns={'Name of Sender':'Name of Other'})
+df2=pd.merge(df2, df1,on='Name of Other', how='left',suffixes={'','_other'})
+
+df2.to_csv("Labeled2.csv")
